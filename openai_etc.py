b73ed268bfee272872720f368c3adb02e710bddc
tanupoo <github-shoichi@tanu.org>
2024-11-10 10:05:44 +0900

    added openai suppliment function

---
+++ openai_etc.py
@@ -0,0 +1,15 @@
+from os import environ
+
+def openai_get_apikey(storage):
+    apikey = None
+    if storage is None:
+        apikey = environ.get("OPENAI_APIKEY")
+    r = re.match(r"env:([\w\d]+)", storage)
+    if r:
+        apikey = environ.get(r.group(1))
+    else:
+        apikey = open(storage).read().strip()
+    if apikey is None:
+        raise ValueError(f"APIKEY is not defined.")
+    return apikey
+
