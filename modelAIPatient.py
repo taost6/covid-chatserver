1baa7152f1dd960e44134c4042642e82f7a54021
tanupoo <github-shoichi@tanu.org>
2024-11-10 21:22:48 +0900

    thread_id is optional

--- modelAIPatient.py
+++ modelAIPatient.py
@@ -4,5 +4,9 @@ from typing import Literal, Union, List, Optional, Any
 class AIPatient(BaseModel):
     user_id: str    = Field(description="Identifier exposed to the peer")
     patient_id: str = Field(description="Patient ID to be communicated.")
-    thread_id: str  = Field(description="Thread Idenfifier of OpenAI API")
+    thread_id: Optional[str] = Field(None,
+            description="Thread Idenfifier of OpenAI API")
 
+if __name__ == "__main__":
+    AIPatient(user_id="aaa", patient_id="bbb")
+    AIPatient(user_id="aaa", patient_id="bbb", thread_id="ccc")
