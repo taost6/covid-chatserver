121467d76b2b931d4fc9feb8b890cf03cb4db145
tanupoo <github-shoichi@tanu.org>
2024-11-17 16:12:18 +0900

    merged modelAIPatient into modelUserDef

--- modelUserDef.py
+++ modelUserDef.py
@@ -1,13 +1,18 @@
 from pydantic import BaseModel, Field
 from typing import Literal, Union, List, Optional, Any
-from modelAIPatient import AIPatient
+
+class AIPatient(BaseModel):
+    user_id: str    = Field(description="Identifier exposed to the peer")
+    patient_id: str = Field(description="Patient ID to be communicated.")
+    thread_id: Optional[str] = Field(None,
+            description="Thread Idenfifier of OpenAI API")
 
 class UserDef(BaseModel):
-    user_id: str
-    role: str
-    status: str
-    ws: Any = None
-    peer: Optional[Union["UserDef",AIPatient]] = Field(
-            None,
-            description="placeholder of the peer information.")
+    user_id: str   = Field(description="User ID")
+    role: str      = Field(description="User Role")
+    status: str    = Field(description="User Status")
+    ws: Any = None = Field(description="Placeholder of WebSocket session")
 
+if __name__ == "__main__":
+    AIPatient(user_id="aaa", patient_id="bbb")
+    AIPatient(user_id="aaa", patient_id="bbb", thread_id="ccc")
