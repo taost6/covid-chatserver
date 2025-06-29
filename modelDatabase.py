85c2a1fa74a1e34ba0a516e252478cc684570cc0
laserbeam2045 <laserbeam2045@gmail.com>
2025-06-29 12:27:12 +0900

    fix: importを修正

--- modelDatabase.py
+++ modelDatabase.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, Column, Integer, String, Text, DateTime
+from sqlalchemy import create_engine, Column, Integer, String, Text, DateTime, Boolean
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.sql import func
