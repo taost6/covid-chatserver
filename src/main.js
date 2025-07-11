2b0c9acfd8ec628ca9da0064139b661275f6bf98
laserbeam2045 <laserbeam2045@gmail.com>
2025-07-11 22:58:29 +0900

    fix: toReversedのポリフィルを作成

--- src/main.js
+++ src/main.js
@@ -4,6 +4,29 @@ import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import router from './router'
 
+// Polyfill for Array.prototype.toReversed for older browsers
+if (!Array.prototype.toReversed) {
+  Array.prototype.toReversed = function() {
+    return this.slice().reverse();
+  };
+}
+
+// Polyfill for Array.prototype.toSorted for older browsers  
+if (!Array.prototype.toSorted) {
+  Array.prototype.toSorted = function(compareFn) {
+    return this.slice().sort(compareFn);
+  };
+}
+
+// Polyfill for Array.prototype.toSpliced for older browsers
+if (!Array.prototype.toSpliced) {
+  Array.prototype.toSpliced = function(start, deleteCount, ...items) {
+    const result = this.slice();
+    result.splice(start, deleteCount, ...items);
+    return result;
+  };
+}
+
 const app = createApp(App)
 const pinia = createPinia()
 
